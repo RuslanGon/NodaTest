88c037b87fb3ed2677c9715937f7888c3ff465ce
RuslanGon <ruslan441006@gmail.com>
2024-06-25 16:33:15 +0300

    wer

--- src/utils/env.js
+++ src/utils/env.js
@@ -2,6 +2,8 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const env = (envName) => {
-return process.env[envName];
+export const env = (envName, defaultValue) => {
+    if(process.env[envName]) return process.env[envName];
+    if(defaultValue) return defaultValue;
+    throw new Error(`Environment variable ${envName} is not defined`);
 };
