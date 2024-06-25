20f4917296d0a3e68495dd4fa4886ae412baf47b
RuslanGon <ruslan441006@gmail.com>
2024-06-25 12:21:47 +0300

    wer

--- src/index.js
+++ src/index.js
@@ -4,20 +4,21 @@ import pino from 'pino-http';
 const app = express();
 
 app.use(pino({
-    transport: 'pino-pretty'
+    transport: {
+        target: 'pino-pretty'
+    }
 }));
 
 app.use((req, res, next) => {
-req.someId = Math.random();
-next();
+    req.someId = Math.random();
+    next();
 });
 
 app.get('/', (req, res, next) => {
     console.log(req.someId);
-res.send('Hello Ruslan Goncharenko !!!');
+    res.send('Hello Ruslan Goncharenko !!!');
 });
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3001');
+    console.log('Server is running on port 3000');
 });
-
