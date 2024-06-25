19aafe082c3b3ea08865da1c3225b47fe0fbf8e9
RuslanGon <ruslan441006@gmail.com>
2024-06-25 13:11:06 +0300

    wer

--- src/index.js
+++ src/index.js
@@ -1,27 +1,3 @@
-import express from 'express';
-import pino from 'pino-http';
-import cors from 'cors';
+import { startServer } from "./server";
 
-const app = express();
-
-app.use(pino({
-    transport: {
-        target: 'pino-pretty'
-    }
-}));
-
-app.use(cors());
-
-
-
-app.get('/', (req, res) => {
-    res.send('Hello Ruslan Goncharenko !!!');
-});
-
-app.use('*', (req, res) => {
-    res.status(404).send('Oops, not found');
-});
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+startServer() ;
---
+++ src/server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+
+
+export const startServer = () => {
+
+    const app = express();
+
+    app.use(pino({
+        transport: {
+            target: 'pino-pretty'
+        }
+    }));
+
+    app.use(cors());
+
+    app.get('/', (req, res) => {
+        res.send('Hello Ruslan Goncharenko !!!');
+    });
+
+    app.use('*', (req, res) => {
+        res.status(404).send('Oops, not found');
+    });
+
+    app.use('*', (error, req, res, next) => {
+    res.status(500).send(error.message);
+    });
+
+    app.listen(3000, () => {
+        console.log('Server is running on port 3000');
+    });
+};
+
