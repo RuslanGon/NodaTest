cc795fcde661835cb28a5e8e43495ae4e7aa8790
RuslanGon <ruslan441006@gmail.com>
2024-06-26 10:23:35 +0300

    wer

--- src/db/initMongoConection.js
+++ src/db/initMongoConection.js
@@ -1,6 +1,12 @@
 import mongoose from "mongoose";
+import {env} from '../utils/env.js';
+import { ENV_VARS } from "../constants";
 
 
 export const initMongoConection = () => {
-const conectionLink = 'mongodb+srv://Ruslan:<password>@cluster0.9yeu1fb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+  const conectionLink = `mongodb+srv://${env(ENV_VARS.MONGODB_USER)}:${env(
+    ENV_VARS.MONGODB_PASSWORD,
+  )}@${env(ENV_VARS.MONGODB_URL)}/${env(
+    ENV_VARS.MONGODB_DB,
+  )}?retryWrites=true&w=majority&appName=Cluster0`;
 };
