ef4661fa8547dc8c71f28b63305f1481693c4b5d
RuslanGon <ruslan441006@gmail.com>
2024-06-26 11:26:09 +0300

    wer

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import { ENV_VARS } from './constants.js';
 const app = express();
 const PORT = env(ENV_VARS.PORT, 3000);
 
+
 export const startServer = () => {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
