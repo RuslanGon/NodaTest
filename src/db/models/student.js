316aec873c6714f00f43071fef34ee19a3d3f3f4
RuslanGon <ruslan441006@gmail.com>
2024-06-26 11:45:46 +0300

    wer

---
+++ src/db/models/student.js
@@ -0,0 +1,10 @@
+import { Schema } from "mongoose";
+
+const studentShema = new Schema(
+{name: {type: String, required: true },
+age: { type: Number, require: true},
+gender: { type: String, require: true, enum: ['male', 'female', 'other' ] },
+avgMark: { type: Number, require: true, min: 1, max: 12 },
+onDuty: { type: Boolean, default: false }
+}
+);
