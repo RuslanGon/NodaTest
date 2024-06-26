63f091d94e124b54d3bfe4f32e4f9f85733c319e
RuslanGon <ruslan441006@gmail.com>
2024-06-26 10:30:05 +0300

    wer

--- src/db/initMongoConection.js
+++ src/db/initMongoConection.js
@@ -1,12 +1,17 @@
-import mongoose from "mongoose";
-import {env} from '../utils/env.js';
-import { ENV_VARS } from "../constants";
+import mongoose from 'mongoose';
+import { env } from '../utils/env.js';
+import { ENV_VARS } from '../constants';
 
-
-export const initMongoConection = () => {
+export const initMongoConection = async () => {
   const conectionLink = `mongodb+srv://${env(ENV_VARS.MONGODB_USER)}:${env(
     ENV_VARS.MONGODB_PASSWORD,
   )}@${env(ENV_VARS.MONGODB_URL)}/${env(
     ENV_VARS.MONGODB_DB,
   )}?retryWrites=true&w=majority&appName=Cluster0`;
+
+  try {
+    await mongoose.connect(conectionLink);
+  } catch (err) {
+    console.log(err);
+  }
 };
