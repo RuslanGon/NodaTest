eb1c7e09d4df8e15b5b62f8da2dfe7977a702157
RuslanGon <ruslan441006@gmail.com>
2024-06-27 15:16:06 +0300

    wer

--- src/routers/students.js
+++ src/routers/students.js
@@ -1,11 +1,10 @@
-import { Router } from "express";
-import { getAllStudents, getStudentById } from "../services/students.js";
-import { getStudentByIdController, getStudentsController } from "../controllers/students.js";
+import { Router } from 'express';
+import { getStudentByIdController, getStudentsController } from '../controllers/students.js';
 
 const studentsRouter = Router();
 
 studentsRouter.get('/students', getStudentsController);
 
-  studentsRouter.get('/students/:studentId', getStudentByIdController);
+studentsRouter.get('/students/:studentId', getStudentByIdController);
 
-  export default studentsRouter;
+export default studentsRouter;
