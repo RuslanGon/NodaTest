c01281fb3ee82fd756f28d0b47b761c8dc0725be
RuslanGon <ruslan441006@gmail.com>
2024-06-29 15:26:03 +0300

    wer

--- src/validation/createStudentSchema.js
+++ src/validation/createStudentSchema.js
@@ -1,7 +1,9 @@
 import Joi from 'joi';
 
 export const createStudentSchema = Joi.object({
-name: Joi.string().required().min(3).max(20),
+name: Joi.string().required().min(3).max(20).messages({
+"string.min":"Min string lenght not achived"
+}),
 age: Joi.number().integer().required().min(6).max(18),
 gender: Joi.string().required().valid('male', 'female', 'other' ),
 avgMark: Joi.number().required().min(2).max(18),
