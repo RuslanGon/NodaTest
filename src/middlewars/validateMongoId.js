55b68ae60646304c26da27e0d8160a51817860a8
RuslanGon <ruslan441006@gmail.com>
2024-06-29 12:45:32 +0300

    wer

--- src/middlewars/validateMongoId.js
+++ src/middlewars/validateMongoId.js
@@ -2,7 +2,7 @@ import createHttpError from "http-errors";
 import { Types } from "mongoose";
 
 
-export const validateMongoId = (req, res, next) => {
+export const validateMongoId = () =>  (req, res, next) => {
 const id = req.params.studentId;
 if (!Types.ObjectId.isValid(id)) {
     return next(createHttpError(400, 'Invalid Id'));
