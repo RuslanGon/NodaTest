0747a7a1a0de5a8ea2ec0f324972034fe57a3d63
RuslanGon <ruslan441006@gmail.com>
2024-06-29 13:35:03 +0300

    wer

--- src/validation/createStudentSchema.js
+++ src/validation/createStudentSchema.js
@@ -1,1 +1,10 @@
-export const createStudentSchema = ()
+import Joi from 'joi';
+
+export const createStudentSchema = Joi.object({
+name: Joi.string().required().min(3).max(20),
+age: Joi.number().integer().required().min(6).max(18),
+gender: Joi.string().required().valid('male', 'female', 'other' ),
+avgMark: Joi.number().required().min(2).max(18),
+onDuty: Joi.Boolean(),
+});
+
