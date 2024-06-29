9b37e31608a7ffeea45d0f8064476076c249fb53
RuslanGon <ruslan441006@gmail.com>
2024-06-29 12:50:51 +0300

    wer

--- src/middlewars/validateMongoId.js
+++ src/middlewars/validateMongoId.js
@@ -2,8 +2,12 @@ import createHttpError from "http-errors";
 import { Types } from "mongoose";
 
 
-export const validateMongoId = () =>  (req, res, next) => {
-const id = req.params.studentId;
+export const validateMongoId = (idName = 'id') =>  (req, res, next) => {
+const id = req.params[idName];
+if(!id){
+throw new Error('id in validateMongoId is not faund');
+}
+
 if (!Types.ObjectId.isValid(id)) {
     return next(createHttpError(400, 'Invalid Id'));
   }
