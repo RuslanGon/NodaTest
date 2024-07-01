787ac1470519b54e3a668f26f00cd184ce740f58
RuslanGon <ruslan441006@gmail.com>
2024-07-01 13:09:38 +0300

    wer

--- src/controllers/students.js
+++ src/controllers/students.js
@@ -4,7 +4,9 @@ import { parsePaginationParams } from "../utils/parsePaginationParams.js";
 
 export const getStudentsController = async (req, res) => {
 const { page, perPage } = parsePaginationParams(req.query);
-    const students = await getAllStudents({ page, perPage });
+const { sortBy, sortOrder } = req.query;
+
+    const students = await getAllStudents({ page, perPage, sortBy, sortOrder });
     res.json({
       status: 200,
       message: 'get all students',
